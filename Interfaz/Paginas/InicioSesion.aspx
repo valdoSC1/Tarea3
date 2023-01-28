ba477e60377ebb1720fd64529b1d91956aa8225e
Andrey Monge Raabe <andrey.monge.raabe@cuc.cr>
2023-01-28 16:22:48 -0600

    Login v.1

---
+++ Interfaz/Paginas/InicioSesion.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="InicioSesion.aspx.cs" Inherits="Interfaz.Paginas.InicioSesion" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+
+            <input runat="server" type="text" id="txtUsuario" class="fadeIn second" name="login" placeholder="Usuario">
+            <input runat="server" type="text" id="txtContrasena" class="fadeIn third" name="pass" placeholder="Contraseña">
+            <asp:Button ID="btnInicioSesion" runat="server" Text="Iniciar Sesión" />
+
+        </div>
+    </div>
+</asp:Content>
---
+++ Interfaz/Paginas/InicioSesion.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Interfaz.Paginas
+{
+
+
+    public partial class InicioSesion
+    {
+
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtUsuario;
+
+        /// <summary>
+        /// Control txtContrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtContrasena;
+
+        /// <summary>
+        /// Control btnInicioSesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnInicioSesion;
+    }
+}
